d76c3dd1bc1602180eafe5b41171a74a361cde55
Radhika <radhikakumari0508@gmail.com>
2023-08-19 12:07:45 +0530

    display X whwn clicking on a square

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 import './styles.scss';
 import Board from './components/Board';
 
 function App(){
 
-  const [counter, setCounter] = useState(1)
+  // const [counter, setCounter] = useState(1)
 
-  const onBtnClick = () => {
-    setCounter(currentCounter => {
-      return currentCounter + 1;
-    });
+  // const onBtnClick = () => {
+  //   setCounter(currentCounter => {
+  //     return currentCounter + 1;
+  //   });
    
-  };
+  // };
 
   return (
     <div className='app'>
-      <div>
+      {/* <div>
         <button onClick={onBtnClick}>Click me please</button>
         <div>{counter}</div>
-      </div>
+      </div> */}
+      <Board />
     </div>
   );
 }
